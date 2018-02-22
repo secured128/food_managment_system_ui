a483a1d4695b9249fe6f9266abb48b69d2170720
Alex Bromberg <alexb@traiana.com>
2018-02-22 10:48:41 +0200

    spring boot with vaadin UI

---
+++ src/main/java/com/elalex/User.java
@@ -0,0 +1,74 @@
+package com.elalex;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.persistence.*;
+import java.io.IOException;
+import java.io.Serializable;
+import java.io.StringWriter;
+
+@JsonPropertyOrder({"id", "name", "password"})
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class User implements Serializable {
+
+    @JsonIgnore
+    private ObjectMapper jsonMapper = new ObjectMapper();
+
+    @JsonProperty("id")
+    private long id;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("password")
+    private String password;
+
+    protected User() {
+    }
+
+    public User(String name, String password) {
+        this.setName(name);
+        this.setPassword(password);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        StringWriter sw = new StringWriter();
+        try {
+            new ObjectMapper().writeValue(sw, this);
+            return sw.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
