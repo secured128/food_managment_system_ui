c14a980a390c60fb4ada36a1df75c07db146271d
secured128 <secured128@gmail.com>
2018-02-22 19:46:04 +0200

    working client example

--- src/main/java/com/elalex/Application.java
+++ src/main/java/com/elalex/Application.java
@@ -29,43 +29,44 @@ public class Application {
     public CommandLineRunner loadData(RestTemplate restTemplate) {
         return (args) -> {
 //            // save a couple of customers
-//            repository.save(new Customer("Jack", "Bauer"));
-//            repository.save(new Customer("Chloe", "O'Brian"));
-//            repository.save(new Customer("Kim", "Bauer"));
-//            repository.save(new Customer("David", "Palmer"));
-//            repository.save(new Customer("Michelle", "Dessler"));
+//            repository.save(new User("Jack", "Bauer"));
+//            repository.save(new User("Chloe", "O'Brian"));
+//            repository.save(new User("Kim", "Bauer"));
+//            repository.save(new User("David", "Palmer"));
+//            repository.save(new User("Michelle", "Dessler"));
 //
 //            // fetch all customers
-//            log.info("Customers found with findAll():");
+//            log.info("Users found with findAll():");
 //            log.info("-------------------------------");
-//            for (Customer customer : repository.findAll()) {
+//            for (User customer : repository.findAll()) {
 //                log.info(customer.toString());
 //            }
 //            log.info("");
 //
 //            // fetch an individual customer by ID
-//            Customer customer = repository.findOne(1L);
-//            log.info("Customer found with findOne(1L):");
+//            User customer = repository.findOne(1L);
+//            log.info("User found with findOne(1L):");
 //            log.info("--------------------------------");
 //            log.info(customer.toString());
 //            log.info("");
 //
 //            // fetch customers by last name
-//            log.info("Customer found with findByLastNameStartsWithIgnoreCase('Bauer'):");
+//            log.info("User found with findByLastNameStartsWithIgnoreCase('Bauer'):");
 //            log.info("--------------------------------------------");
-//            for (Customer bauer : repository
+//            for (User bauer : repository
 //                    .findByLastNameStartsWithIgnoreCase("Bauer")) {
 //                log.info(bauer.toString());
 //            }
 //            log.info("");
 
-            User user = new User("alex", "alex");
+            User userToFind = new User("ella", "ella");
             try {
 
-                ResponseEntity<User> responseEntity = restTemplate.postForEntity("http://localhost:8090/login/", user,User.class);
+                ResponseEntity<User> responseEntity = restTemplate.postForEntity("http://localhost:8090/login/", userToFind, User.class);
+                User user = responseEntity.getBody();
 //                User user1 = restTemplate.getForObject(
 //                        "http://localhost:8090/user/71269", User.class);
-                log.info(responseEntity.toString());
+                log.info(user.toString());
             } catch (IllegalStateException e) {
                 log.info(e.getMessage());
             } catch (HttpClientErrorException e) {
@@ -75,7 +76,7 @@ public class Application {
                         log.error("Invalid input was used instead valid user data");
                         break;
                     case NOT_FOUND:
-                        log.error("Can not fined user : " + user.toString());
+                        log.error("Can not fined user : " + userToFind.toString());
                         break;
                     default:
                         break;
