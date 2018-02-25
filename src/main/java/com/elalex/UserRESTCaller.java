f7540a657336b85f286b1f23ee26ad9031f4f51e
secured128 <secured128@gmail.com>
2018-02-25 20:16:11 +0200

    find all converted to async

--- src/main/java/com/elalex/UserRESTCaller.java
+++ src/main/java/com/elalex/UserRESTCaller.java
@@ -4,34 +4,42 @@ import com.vaadin.ui.Notification;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.web.client.AsyncRestTemplate;
 import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 @Service
 public class UserRESTCaller {
     private static final Logger log = LoggerFactory.getLogger(UserRESTCaller.class);
 
-    //    User userToFind = new User("ella", "ella");
-//
-    @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
-    }
-
+    /**
+     * https://www.concretepage.com/spring-4/spring-4-asyncresttemplate-listenablefuture-example
+     *
+     * @return
+     */
     public List<User> findAll() {
+        AsyncRestTemplate asycTemp = new AsyncRestTemplate();
+        String url = "http://localhost:8090/find";
+        HttpMethod method = HttpMethod.GET;
+        Class<List> responseType = List.class;
+        ListenableFuture<ResponseEntity<List>> future = asycTemp.exchange(url, method, HttpEntity.EMPTY, responseType);
         try {
-            ResponseEntity<List> responseEntity = new RestTemplateBuilder().build().getForEntity("http://localhost:8090/find", List.class);
+            ResponseEntity<List> responseEntity = future.get();
             return getUsersListFromMapsList(responseEntity.getBody());
-        } catch (Exception e) {
-            log.error(e.getMessage());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
         }
         return null;
     }
